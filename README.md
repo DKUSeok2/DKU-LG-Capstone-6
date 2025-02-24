f2b71e45aba67301e8bf9f70785dd64e07c6cfee
Downy-newlearner <jdh251425142514@gmail.com>
2025-02-24 21:01:09 +0900

    cover_nan 업데이트

--- README.md
+++ README.md
@@ -24,3 +24,77 @@ train = pd.read_csv("../shared codes/data/train.csv")
 # missing_value_removal_function 사용
 df = missing_value_removal_function(train)
 ```
+
+## IVF vs DI 분리한 데이터 로드하는 방법
+```
+import sys
+import os
+import pandas as pd
+
+# 현재 작업 디렉토리 경로를 가져와 shared codes 폴더의 위치를 sys.path에 추가합니다.
+# sys.path에 추가된 경로에 있는 py 폴더는 임포트할 수 있다.
+current_dir = os.getcwd()
+shared_codes_dir = os.path.join(current_dir, '../shared codes')
+sys.path.append(shared_codes_dir)
+
+
+# cover_nan 모듈을 임포트
+from cover_nan_0224_IVFDI import missing_value_removal_function
+
+# 원본 train 데이터 로드
+train = pd.read_csv("../shared codes/data/train.csv")
+test = pd.read_csv("../shared codes/data/test.csv")
+
+# missing_value_removal_function 사용
+train_IVF, train_DI = missing_value_removal_function(train)
+test_IVF, test_DI = missing_value_removal_function(test)
+```
+
+## 난자 나이 기준으로 분리한 데이터 로드하는 방법
+```
+import sys
+import os
+import pandas as pd
+
+# 현재 작업 디렉토리 경로를 가져와 shared codes 폴더의 위치를 sys.path에 추가합니다.
+# sys.path에 추가된 경로에 있는 py 폴더는 임포트할 수 있다.
+current_dir = os.getcwd()
+shared_codes_dir = os.path.join(current_dir, '../shared codes')
+sys.path.append(shared_codes_dir)
+
+
+# cover_nan 모듈을 임포트
+from cover_nan_0215_dahun import missing_value_removal_function
+
+# 원본 train 데이터 로드
+train = pd.read_csv("../shared codes/data/train.csv")
+test = pd.read_csv("../shared codes/data/test.csv")
+
+# missing_value_removal_function 사용
+train_young, train_middle, train_old, train_unknown = missing_value_removal_function(train)
+test_young, test_middle, test_old, test_unknown = missing_value_removal_function(test)
+```
+
+## 난자 나이, IVF/DI, 배아 생성 주요 이유를 기준으로 분리한 데이터 로드하는 방법
+```import sys
+import os
+import pandas as pd
+
+# 현재 작업 디렉토리 경로를 가져와 shared codes 폴더의 위치를 sys.path에 추가합니다.
+# sys.path에 추가된 경로에 있는 py 폴더는 임포트할 수 있다.
+current_dir = os.getcwd()
+shared_codes_dir = os.path.join(current_dir, '../shared codes')
+sys.path.append(shared_codes_dir)
+
+
+# cover_nan 모듈을 임포트
+from cover_nan_0224_age_IVFDI import missing_value_removal_function
+
+# 원본 train 데이터 로드
+train = pd.read_csv("../shared codes/data/train.csv")
+test = pd.read_csv("../shared codes/data/test.csv")
+
+# missing_value_removal_function 사용
+train_young_IVF, train_young_DI, train_middle_IVF, train_middle_DI, train_old_IVF, train_old_DI, train_unknown, train_0 = missing_value_removal_function(train)
+test_young_IVF, test_young_DI, test_middle_IVF, test_middle_DI, test_old_IVF, test_old_DI, test_unknown, test_0 = missing_value_removal_function(test)
+```
--- shared codes/cover_nan_0224_IVFDI.py
+++ shared codes/cover_nan_0224_IVFDI.py
@@ -91,22 +91,58 @@ def preprocess_columns_yooseok(df):
 
 def cover_nan_hoyeong(df):
     # 1. '난자 출처'의 결측값을 '본인 제공'으로 채우기
-    df['난자 출처'].replace('알 수 없음','본인 제공')
+    df['난자 출처'].replace('알 수 없음','본인 제공', inplace=True)
 
     # 2. '난자 기증자 나이' 결측값을 시술 당시 나이로 채우기
     df.loc[df['난자 기증자 나이'] == '알 수 없음', '난자 기증자 나이'] = df['시술 당시 나이']
 
     # 3. '파트너 정자와 혼합된 난자 수'의 결측값을 0으로 채우기
-    df['파트너 정자와 혼합된 난자 수'].fillna(0)
+    df['파트너 정자와 혼합된 난자 수'].fillna(0, inplace=True)
 
     # 4. '기증자 정자와 혼합된 난자 수'의 결측값을 1로 채우기
-    df['기증자 정자와 혼합된 난자 수'].fillna(1)
+    df['기증자 정자와 혼합된 난자 수'].fillna(1, inplace=True)
 
     # 5. '동결 배아 사용 여부', '신선 배아 사용 여부', '기증 배아 사용 여부'의 결측값을 0으로 채우기
     for column in ['동결 배아 사용 여부', '신선 배아 사용 여부', '기증 배아 사용 여부']:
-        df[column].fillna(0)
+        df[column].fillna(0, inplace=True)
     
     return df
+
+
+def cover_nan_hoyeong_v2(df):
+    # 1. '난자 출처'의 결측값을 '본인 제공'으로 채우기
+    df['난자 출처'] = df['난자 출처'].replace('알 수 없음', '본인 제공')
+
+    # 2. '난자 기증자 나이' 결측값을 시술 당시 나이로 채우기
+    df.loc[df['난자 기증자 나이'] == '알 수 없음', '난자 기증자 나이'] = df['시술 당시 나이']
+
+    # 3. '파트너 정자와 혼합된 난자 수'의 결측값을 0으로 채우기
+    df['파트너 정자와 혼합된 난자 수'] = df['파트너 정자와 혼합된 난자 수'].fillna(0)
+
+    # 4. '기증자 정자와 혼합된 난자 수'의 결측값을 1로 채우기
+    df['기증자 정자와 혼합된 난자 수'] = df['기증자 정자와 혼합된 난자 수'].fillna(1)
+
+    # 5. '동결 배아 사용 여부', '신선 배아 사용 여부', '기증 배아 사용 여부'의 결측값을 0으로 채우기
+    for column in ['동결 배아 사용 여부', '신선 배아 사용 여부', '기증 배아 사용 여부']:
+        df[column] = df[column].fillna(0)
+    
+    return df
+
+
+def categorize_egg_age(row):
+    # 난자 나이 통합
+    egg_age = row['난자 기증자 나이'] if '난자 기증자 나이' in row else '알 수 없음'
+
+    if egg_age in ['만21-25세', '만26-30세', '만31-35세', '만18-34세']:
+        category = '건강한 난자'
+    elif egg_age in ['만35-37세', '만38-39세']:
+        category = '노화가 진행 중인 난자'
+    elif egg_age in ['만40-42세', '만43-44세', '만45-50세']:
+        category = '노화된 난자'
+    else:
+        category = '알 수 없음'
+    
+    return category
 
 
 def missing_value_removal_function(df):
@@ -115,7 +151,7 @@ def missing_value_removal_function(df):
     
     파라미터:
         df (pd.DataFrame): NaN을 0으로 대체할 데이터프레임.
-
+ 
     리턴:
         pd.DataFrame: NaN이 0으로 대체된 새로운 데이터프레임.
     """
@@ -128,7 +164,39 @@ def missing_value_removal_function(df):
 
     # 유석 함수
     df = preprocess_columns_yooseok(df)
-    # 호영 함수
-    df = cover_nan_hoyeong(df)
 
-    return df
+    # 호영 함수
+    df = cover_nan_hoyeong_v2(df)
+
+
+
+    # 난자 나이 카테고리화 적용
+    # df['난자 나이 카테고리'] = df.apply(categorize_egg_age, axis=1)
+
+
+    # # 불필요한 컬럼 삭제
+    # # df = df.drop(columns=['시술 당시 나이', '정자 기증자 나이', '난자 기증자 나이', '난자 출처', '정자 출처'])
+    # df = df.drop(columns=['시술 당시 나이', '정자 기증자 나이', '난자 기증자 나이'])
+
+
+    # df['idx'] = df.index
+
+    # 데이터프레임 4개로 쪼개기
+    # df_young = df[df['난자 나이 카테고리'] == '건강한 난자'].drop(columns=['난자 나이 카테고리'])
+    # df_middle = df[df['난자 나이 카테고리'] == '노화가 진행 중인 난자'].drop(columns=['난자 나이 카테고리'])
+    # df_old = df[df['난자 나이 카테고리'] == '노화된 난자'].drop(columns=['난자 나이 카테고리'])
+    # df_unknown = df[df['난자 나이 카테고리'] == '알 수 없음'].drop(columns=['난자 나이 카테고리'])
+
+    # # 데이터프레임 3개로 쪼개기
+    # df_young = df[df['난자 나이 카테고리'] == '건강한 난자'].drop(columns=['난자 나이 카테고리'])
+    # df_old = df[df['난자 나이 카테고리'] == '노화가 진행 중인 난자', '노화된 난자'].drop(columns=['난자 나이 카테고리'])
+    # df_unknown = df[df['난자 나이 카테고리'] == '알 수 없음'].drop(columns=['난자 나이 카테고리'])
+
+    df_IVF = df[df['시술 유형'] == 'IVF']
+    df_DI = df[df['시술 유형'] == 'DI']
+
+
+    return df_IVF, df_DI
+
+
+    # return df_young, df_old, df_unknown
